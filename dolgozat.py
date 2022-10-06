331033aa07c5d01dc26f5a1bc18f70eba2a30766
Nephilim598 <99308571+Nephilim598@users.noreply.github.com>
2022-10-06 15:51:08 +0200

    Add files via upload
    2022.10.06

---
+++ dolgozat.py
@@ -0,0 +1,31 @@
+a = float(input("Kérem a háromszög oldalának hosszát: "))
+b = float(input("Kérem a háromszög magasságát: "))
+
+x = (b * a) / 2
+
+print("A háromszög területe: ",x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
